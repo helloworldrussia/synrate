e4f2db0b3d881101143cccb075311f1ecf24ebf5
helloworldrussia <coolgrifers@gmail.com>
2022-04-05 12:18:05 +0500

    ...

--- synrate/wsgi.py
+++ synrate/wsgi.py
@@ -2,10 +2,11 @@ import os
 import threading
 from django.core.wsgi import get_wsgi_application
 
-from custom_timer import timer
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'synrate.settings')
 
 application = get_wsgi_application()
+
+from custom_timer import timer
+
 t1 = threading.Thread(target=timer)
 t1.start()
