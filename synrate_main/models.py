55f09987b487436d828c7771c203a6b2597da99c
helloworldrussia <coolgrifers@gmail.com>
2022-05-12 17:12:23 +0500

    ...

--- synrate_main/models.py
+++ synrate_main/models.py
@@ -35,7 +35,7 @@ class ParserDetail(models.Model):
 class Offer(models.Model):
     name = models.CharField(max_length=1500, null=True, blank=True, default=None, verbose_name="Название")
     home_name = models.CharField(max_length=200, null=True, blank=True, default=None, verbose_name="Источник")
-    slug = models.CharField(max_length=255, blank=True, null=True, default=None, verbose_name='ЧПУ')
+    slug = models.CharField(max_length=255, uniq=True, blank=True, null=True, default=None, verbose_name='ЧПУ')
     url = models.CharField(max_length=599, null=True, blank=True, default=None,
                            verbose_name="Ссылка на источник")
     location = models.CharField(max_length=250, null=True, blank=True, default="РФ", verbose_name="Регион")
