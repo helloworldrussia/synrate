030bb244de903fec7dfe266a6284c3d4bda59381
helloworldrussia <coolgrifers@gmail.com>
2022-05-13 14:34:40 +0500

    change detail view url

--- synrate_main/urls.py
+++ synrate_main/urls.py
@@ -8,11 +8,11 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('parser_admin/', views.parser_admin, name='parser_admin'),
     path('offer_list/<int:pk>', views.list, name="list"),
-    path('offer_detail/<int:pk>', views.detail, name="detail"),
+    # path('offer_detail/<int:pk>', views.detail, name="detail"),
     path('ostatki/<str:args>', views.search, name="search"),
     path('ostatki', views.search_all, name="search_all"),
     path('categories', views.category, name="category_list"),
-    path('detail/<str:id>', views.detail_info, name="detail_view"),
+    path('offer/id/<str:id>', views.detail_info, name="detail_view"),
     # re_path(r'^filter/$', views.listing, name="filtration"),
 
     re_path(r'^offers/$', views.listing, name="filtration"),
